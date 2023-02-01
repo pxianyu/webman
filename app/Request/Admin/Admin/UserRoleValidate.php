18ae010f46d0758d41d3165b63e84819dd1a6342
咸鱼er了 <540197556@qq.com>
2023-02-01 17:33:32 +0900

    增加授权时数据存在验证

--- app/Request/Admin/Admin/UserRoleValidate.php
+++ app/Request/Admin/Admin/UserRoleValidate.php
@@ -9,8 +9,8 @@ class UserRoleValidate extends BaseValidate
     public function getRulesByEmpower(): array
     {
         return [
-            'admin_id'  =>  'required|integer',
-            'role_id'  =>  'required|integer'
+            'admin_id'  =>  'required|integer|exists:admins,id',
+            'role_id'  =>  'required|integer|exists:roles,id'
         ];
     }
     public function messages(): array
@@ -19,7 +19,9 @@ class UserRoleValidate extends BaseValidate
             'admin_id.required' => '用户id不能为空',
             'username.integer'     => '用户id必须为整数',
             'role_id.required'   => '角色id不能为空',
-            'role_id.integer'   => '角色id必须为整数'
+            'role_id.integer'   => '角色id必须为整数',
+            'role_id.exists'   => '角色不存在',
+            'admin_id.exists'   => '要授权的用户不存在',
         ];
     }
 }

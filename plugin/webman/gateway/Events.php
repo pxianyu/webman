3564929a8e3ee5f5624a4aacde09b1b8e86e56ff
咸鱼er了 <540197556@qq.com>
2023-02-03 10:29:53 +0900

    增加GatewayWorker

--- plugin/webman/gateway/Events.php
+++ plugin/webman/gateway/Events.php
@@ -3,22 +3,23 @@
 namespace plugin\webman\gateway;
 
 use GatewayWorker\Lib\Gateway;
+use support\Log;
 
 class Events
 {
     public static function onWorkerStart($worker)
     {
-
+        Log::error('GatewayWorker started');
     }
 
     public static function onConnect($client_id)
     {
-
+        Log::error('GatewayWorker onConnect');
     }
 
     public static function onWebSocketConnect($client_id, $data)
     {
-
+        Log::error('GatewayWorker onWebSocketConnect');
     }
 
     public static function onMessage($client_id, $message)
@@ -28,7 +29,7 @@ class Events
 
     public static function onClose($client_id)
     {
-
+        Log::error('GatewayWorker onClose');
     }
 
 }
